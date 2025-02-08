c0e1ce4c19a1e3b2a3b647377825747a5d796204
perilka <lisetskaya.evgeniya@yandex.ru>
2025-02-08 13:24:23 +0300

    issue is solved

--- CSV_issue.py
+++ CSV_issue.py
@@ -1,6 +1,8 @@
 import csv
 
 
+# Задание 1
+
 with open('prices.txt', 'r', encoding='utf8') as f:
     data = []
     for line in f:
@@ -9,6 +11,25 @@ with open('prices.txt', 'r', encoding='utf8') as f:
 
 print(data)
 
-with open('prices.csv', 'w') as csvfile:
+with open('prices.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(data)
+
+
+# Задание 2
+
+with open('prices.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    lst = list(reader)
+
+print(lst)
+
+def price_counter(shopping_list):
+    prices = 0
+    for i in shopping_list:
+        price = int(i[1])*int(i[2])
+        prices += price
+    return prices
+
+
+print(price_counter(lst))
