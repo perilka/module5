fd8a7ca7e09f4fd4bc7cf5057586c28470f7070e
perilka <lisetskaya.evgeniya@yandex.ru>
2025-02-13 02:01:04 +0300

    issue is solved (probably)

---
+++ JSON_issue.py
@@ -0,0 +1,14 @@
+import csv, json
+from pprint import pprint
+
+with open('данные.csv', 'r') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    python_obj = []
+    json_data = []
+    for row in reader:
+        python_obj.append(row)
+        json_data.append(json.dumps(row))
+
+pprint(python_obj, indent=4)
+print()
+pprint(json_data, indent=4)
